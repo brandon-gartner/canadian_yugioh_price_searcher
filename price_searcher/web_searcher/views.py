b9a8c9eb23e6871a36cc8c658a5db3fb758d7bb4
Brandon Gartner <brandonofthegartner@gmail.com>
2021-05-30 09:53:54 -0400

    basic page set in place

--- price_searcher/web_searcher/views.py
+++ price_searcher/web_searcher/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello, this is the first live page.")
+def main_page(request):
+    template = loader.get_template('main_page.html')
+    context = {
+
+    }
+    return HttpResponse(template.render(context, request))
