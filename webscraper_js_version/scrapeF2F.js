e419adfb34517fa2e25e056111b8869c72070b67
valcon <brandonofthegartner@gmail.com>
2021-07-31 10:23:59 -0400

    fixed facetoface scanning so that it now obtains links

--- webscraper_js_version/scrapeF2F.js
+++ webscraper_js_version/scrapeF2F.js
@@ -17,23 +17,20 @@ const getFaceToFace = async (cardNameSearch) => {
         let cardInfo = $('ul > li > article')
         let cardText = cardInfo.find('div.card-body > h4.card-title > a')
         let cardPrice = cardInfo.find('div.card-text > div.card-addtoCart > div.card-price > div.primary-price--withoutTax > span.price--withoutTax')
+        let cardLink = cardInfo.find('figure.card-figure > a')
 
         //transforming the data into JS arrays so I can use it more easily.
         var cardTextArray = cardText.toArray().map((element) => { return $(element).text().trim() });
         var cardPriceArray = cardPrice.toArray().map((element) => { return $(element).text() });
-
-        //will remove these, still useful for testing right now
-        // console.log(cardTextArray)
-        // console.log(cardPriceArray)
+        var cardLinkArray = cardLink.toArray().map((element) => {return $(element) });
         
         //fills the array with JS objects containing the card's title/set, its price, and the seller who is selling it.
-        //TODO: add a link to the listing, which I can redirect users to if they click
         for (var i = 0; i < cardText.length; i++){
             var listing = {
                 title: cardTextArray[i],
                 price: cardPriceArray[i],
                 seller: "facetofacegames.com",
-                //TODO: add link
+                link: cardLinkArray[i].attr('href'),
             }
             cardListings.push(listing);
         }
